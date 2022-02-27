a947fc6cdd64f8c24e8f76ecde5853ed8f7a616b
Honza Sládek <me@honzasladek.com>
2022-02-27 10:21:32 +0100

    Add HTML email template (by Tomáš Herma)

--- src/email.ts
+++ src/email.ts
@@ -42,14 +42,100 @@ const templates: Templates = {
 			Tým Pomozme Ukrajině
 		`,
 		html: `
-			<p>Dobrý den,</p>
+			<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+			<html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
 
-			<p>děkujeme za nabídku pomoci. Prosíme vás o potvrzení vašeho emailu kliknutím na odkaz níže a následně nastavení hesla.</p>
+			<head>
+			<!--[if gte mso 9]>
+			<xml>
+			<o:OfficeDocumentSettings>
+				<o:AllowPNG/>
+				<o:PixelsPerInch>96</o:PixelsPerInch>
+			</o:OfficeDocumentSettings>
+			</xml>
+			<![endif]-->
+			<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<meta name="x-apple-disable-message-reformatting">
+			<!--[if !mso]><!-->
+			<meta http-equiv="X-UA-Compatible" content="IE=edge">
+			<!--<![endif]-->
+			<title></title>
+			</head>
 
-			<p><a href="${payload.verificationUrl}">${payload.verificationUrl}</a></p>
+			<body class="clean-body u_body" style="margin: 0;padding: 0;-webkit-text-size-adjust: 100%;background-color: #ffffff;color: #000000">
+			<!--[if IE]><div class="ie-container"><![endif]-->
+			<!--[if mso]><div class="mso-container"><![endif]-->
+			<table
+				style="border-collapse: collapse;table-layout: fixed;border-spacing: 0;mso-table-lspace: 0pt;mso-table-rspace: 0pt;vertical-align: top;min-width: 320px;Margin: 0 auto;background-color: #ffffff;width:100%"
+				cellpadding="0" cellspacing="0">
+				<tbody>
+				<tr style="vertical-align: top">
+					<td style="word-break: break-word;border-collapse: collapse !important;vertical-align: top">
+					<!--[if (mso)|(IE)]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td align="center" style="background-color: #e7e7e7;"><![endif]-->
 
-			<p>Děkujeme!</p>
-			<p>Tým Pomozme Ukrajině</p>
+
+					<div class="u-row-container" style="padding: 0px;background-color: transparent">
+						<div class="u-row"
+						style="Margin: 0 auto;min-width: 320px;max-width: 500px;overflow-wrap: break-word;word-wrap: break-word;word-break: break-word;background-color: transparent;">
+						<div style="border-collapse: collapse;display: table;width: 100%;background-color: transparent;">
+							<!--[if (mso)|(IE)]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td style="padding: 0px;background-color: transparent;" align="center"><table cellpadding="0" cellspacing="0" border="0" style="width:500px;"><tr style="background-color: transparent;"><![endif]-->
+
+							<!--[if (mso)|(IE)]><td align="center" width="500" style="width: 500px;padding: 0px;border-top: 0px solid transparent;border-left: 0px solid transparent;border-right: 0px solid transparent;border-bottom: 0px solid transparent;" valign="top"><![endif]-->
+							<div class="u-col u-col-100" style="max-width: 320px;min-width: 500px;display: table-cell;vertical-align: top;">
+							<div style="width: 100% !important;">
+								<!--[if (!mso)&(!IE)]><!-->
+								<div
+								style="padding: 0px;border-top: 0px solid transparent;border-left: 0px solid transparent;border-right: 0px solid transparent;border-bottom: 0px solid transparent;">
+								<!--<![endif]-->
+
+								<table style="font-family:ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;" role="presentation" cellpadding="0" cellspacing="0"
+									width="100%" border="0">
+									<tbody>
+									<tr>
+										<td style="overflow-wrap:break-word;word-break:break-word;padding:10px;font-family:ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;"
+										align="left">
+
+										<div>
+											<h1 style="font-size: 3.75rem; line-height: 1; --tw-text-opacity: 1;color: rgb(17 24 39/var(--tw-text-opacity));">Pomáhej Ukrajině</h1>
+											<p style="--tw-text-opacity: 1; color: rgb(107 114 128/var(--tw-text-opacity));font-size: 1.25rem; line-height: 1.75rem;">Dobrý den,</p>
+
+											<p style="--tw-text-opacity: 1; color: rgb(107 114 128/var(--tw-text-opacity)); font-size: 1.25rem; line-height: 1.75rem;">děkujeme za nabídku pomoci. Prosíme vás o potvrzení vašeho emailu kliknutím na odkaz níže a následně
+											nastavení hesla.</p>
+
+											<p><a style="color: #b61a3b;text-decoration: none;font-weight: bold;font-size: 1.25rem; line-height: 1.75rem;" href="${payload.verificationUrl}">${payload.verificationUrl}</a></p>
+
+											<p style="--tw-text-opacity: 1; color: rgb(107 114 128/var(--tw-text-opacity));font-size: 1.25rem; line-height: 1.75rem;">Děkujeme!</p>
+											<p style="--tw-text-opacity: 1; color: rgb(156 163 175/var(--tw-text-opacity));font-size: 1.rem; line-height: 1.5rem;">Tým Pomáhej Ukrajině</p>
+										</div>
+
+										</td>
+									</tr>
+									</tbody>
+								</table>
+
+								<!--[if (!mso)&(!IE)]><!-->
+								</div>
+								<!--<![endif]-->
+							</div>
+							</div>
+							<!--[if (mso)|(IE)]></td><![endif]-->
+							<!--[if (mso)|(IE)]></tr></table></td></tr></table><![endif]-->
+						</div>
+						</div>
+					</div>
+
+
+					<!--[if (mso)|(IE)]></td></tr></table><![endif]-->
+					</td>
+				</tr>
+				</tbody>
+			</table>
+			<!--[if mso]></div><![endif]-->
+			<!--[if IE]></div><![endif]-->
+			</body>
+
+			</html>
 		`
 	})
 }
