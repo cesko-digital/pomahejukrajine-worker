be103b528a2c78e0548e513dd562e31a1883c5c6
miky <jankovsky.michal@gmail.com>
2022-04-17 19:17:43 +0200

    add: stagingScripts single run of sending emails

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,22 @@ import {sendReactions} from "./sendReactions.js";
 
 const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
+export async function processActions() {
+	console.log("Sending volunteer verifications...")
+	await sendVerifications()
+	console.log("Sending reaction verifications...")
+	await sendReactionVerifications()
+	console.log("Sending reactions...")
+	await sendReactions()
+}
 
 async function main() {
+	// noinspection InfiniteLoopJS
 	while (true) {
-		console.log("Sending volunteer verifications...");
-		await sendVerifications()
-		console.log("Sending reaction verifications...");
-		await sendReactionVerifications()
-		console.log("Sending reactions...");
-		await sendReactions()
+		await processActions()
 		await sleep(5000)
 	}
 }
 
+// noinspection JSIgnoredPromiseFromCall
 main()
--- src/stagingScripts.ts
+++ src/stagingScripts.ts
@@ -1,13 +1,12 @@
+import { processActions } from "./index.js";
+import { checkRequiredEnvVars } from "./requiredEnvVars.js";
+
 console.log("S T A G I N G   S C R I P T S")
 
-console.log(JSON.stringify({
-    CONTEMBER_CONTENT_URL: process.env.CONTEMBER_CONTENT_URL,
-    CONTEMBER_TENANT_URL: process.env.CONTEMBER_TENANT_URL,
-    CONTEMBER_TOKEN: process.env.CONTEMBER_TOKEN,
-    EMAIL_HOST: process.env.EMAIL_HOST,
-    EMAIL_PORT: process.env.EMAIL_PORT,
-    EMAIL_USER: process.env.EMAIL_USER,
-    EMAIL_PASSWORD: process.env.EMAIL_PASSWORD,
-    EMAIL_FROM: process.env.EMAIL_FROM,
-    FRONTEND_URL: process.env.FRONTEND_URL,
-}, null, 4))
+await checkRequiredEnvVars()
+
+console.time("processActions")
+
+await processActions()
+
+console.timeEnd("processActions")
