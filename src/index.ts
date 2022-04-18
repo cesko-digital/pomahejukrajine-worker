8f56d6d5d5d10dac9dfa7cfe811a688a9f4b60c0
miky <jankovsky.michal@gmail.com>
2022-04-18 20:03:58 +0200

    fix: staging scripts do not import from index (with side effects)

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,6 @@
-import {sendVerifications} from "./sendVerifications.js";
-import {sendReactionVerifications} from "./sendReactionVerifications.js";
-import {sendReactions} from "./sendReactions.js";
+import {processActions} from "./processActions.js";
 
 const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
-
-export async function processActions() {
-	console.log("Sending volunteer verifications...")
-	await sendVerifications()
-	console.log("Sending reaction verifications...")
-	await sendReactionVerifications()
-	console.log("Sending reactions...")
-	await sendReactions()
-}
 
 async function main() {
 	// noinspection InfiniteLoopJS
--- src/stagingScripts.ts
+++ src/stagingScripts.ts
@@ -1,5 +1,5 @@
-import { processActions } from "./index.js";
 import { checkRequiredEnvVars } from "./requiredEnvVars.js";
+import { processActions } from "./processActions.js";
 
 console.log("S T A G I N G   S C R I P T S")
 
