205675f38fb02926ef8526f66904e81834708a45
Jonáš Nobile <jonas.nobile@icloud.com>
2022-05-17 19:54:42 +0200

    Add bulk translations

--- src/utils.ts
+++ src/utils.ts
@@ -3,3 +3,20 @@ import crypto from "crypto";
 export const generateSecretCode = (): string => {
 	return crypto.randomBytes(16).toString('hex')
 }
+
+export function generateMutation(translations: { text: string, id: string }[]): string {
+	let mutation = `mutation {`
+	for (let index = 0; index < translations.length; index++) {
+		mutation += `
+			update_${index}: updateOfferParameterValue(by: {
+				id: "${translations[index].id}" },
+				data: { specificationUK: ${JSON.stringify(translations[index].text)} })
+				{
+					ok
+					errorMessage
+				}
+		`
+	}
+	mutation += `}`
+	return mutation
+}
