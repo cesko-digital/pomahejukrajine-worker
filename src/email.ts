bf084e30477accba27a0f3f7517cbe402c2b931b
Martin Sikora <martin.sikora.ahoj@gmail.com>
2022-05-31 00:39:09 +0200

    add UK texts into the verification email

--- src/email.ts
+++ src/email.ts
@@ -49,6 +49,16 @@ const templates: Templates = {
 
 			Děkujeme!
 			Tým Pomáhej Ukrajině
+
+			-----
+
+			Доброго дня,
+			дякуємо за пропозицію допомоги. Будь ласка, підтвердьте свою електронну пошту, натиснувши посилання нижче а потім встановіть пароль.
+
+			${payload.verificationUrl}
+
+			Děkujeme!
+			Tým Pomáhej Ukrajině
 		`,
 		html: `
 			<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -111,6 +121,18 @@ const templates: Templates = {
 
 											<p style="--tw-text-opacity: 1; color: rgb(107 114 128/var(--tw-text-opacity)); font-size: 1.25rem; line-height: 1.75rem;">děkujeme za nabídku pomoci. Prosíme vás o potvrzení vašeho emailu kliknutím na odkaz níže a následně
 											nastavení hesla.</p>
+
+											<p><a style="color: #b61a3b;text-decoration: none;font-weight: bold;font-size: 1.25rem; line-height: 1.75rem;" href="${payload.verificationUrl}">${payload.verificationUrl}</a></p>
+
+											<p style="--tw-text-opacity: 1; color: rgb(107 114 128/var(--tw-text-opacity));font-size: 1.25rem; line-height: 1.75rem;">Děkujeme!</p>
+											<p style="--tw-text-opacity: 1; color: rgb(156 163 175/var(--tw-text-opacity));font-size: 1.rem; line-height: 1.5rem;">Tým Pomáhej Ukrajině</p>
+										</div>
+
+										<div style="margin-top: 50px;">
+											<h1 style="font-size: 3.75rem; line-height: 1; --tw-text-opacity: 1;color: rgb(17 24 39/var(--tw-text-opacity));">Pomáhej Ukrajině</h1>
+											<p style="--tw-text-opacity: 1; color: rgb(107 114 128/var(--tw-text-opacity));font-size: 1.25rem; line-height: 1.75rem;">Доброго дня,</p>
+
+											<p style="--tw-text-opacity: 1; color: rgb(107 114 128/var(--tw-text-opacity)); font-size: 1.25rem; line-height: 1.75rem;">дякуємо за пропозицію допомоги. Будь ласка, підтвердьте свою електронну пошту, натиснувши посилання нижче а потім встановіть пароль.</p>
 
 											<p><a style="color: #b61a3b;text-decoration: none;font-weight: bold;font-size: 1.25rem; line-height: 1.75rem;" href="${payload.verificationUrl}">${payload.verificationUrl}</a></p>
 
