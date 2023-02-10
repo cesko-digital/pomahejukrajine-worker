a41e1d38b7dfc0986256b8f150341a0b771d3066
Honza Sládek <me@honzasladek.com>
2023-02-10 17:35:52 +0100

    Index stuff without set status

--- src/indexSearch.ts
+++ src/indexSearch.ts
@@ -172,8 +172,8 @@ function offerToDocument(offer: Offer) {
 					}
 				})
 		),
-		status_name: offer.status?.name,
-		status_name_facet: offer.status?.name,
+		status_name: offer.status ? offer.status.name : "Bez stavu",
+		status_name_facet: offer.status ? offer.status.name : "Bez stavu",
 	}
 }
 
